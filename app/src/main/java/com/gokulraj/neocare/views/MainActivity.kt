d0d8a7c323f953da831dd8860279c06f13b47b83
Gokulraj Venugopal <gokulraj.venugopal010@gmail.com>
2023-10-01 16:56:38 -0400

    Updated Start Screen with Logo

--- app/src/main/java/com/gokulraj/neocare/views/MainActivity.kt
+++ app/src/main/java/com/gokulraj/neocare/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gokulraj.neocare
+package com.gokulraj.neocare.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gokulraj.neocare.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
