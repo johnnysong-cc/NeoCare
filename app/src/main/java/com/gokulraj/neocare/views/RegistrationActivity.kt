6fb7d3aaa3bb1a4303c5ecd8214abeea07a9bd7f
Lea-MM <lmagbal1@my.centennialcollege>
2023-10-23 18:24:22 -0400

    Added Registration UI

---
+++ app/src/main/java/com/gokulraj/neocare/views/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.gokulraj.neocare.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gokulraj.neocare.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
