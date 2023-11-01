7b8c399d132a214c564f3b0937983d39ffa9a9f0
Husna Fatima <hfatima4@my.centennialcollege.ca>
2023-11-01 15:26:29 -0400

    " Code added for voice command"

--- app/src/main/java/com/gokulraj/neocare/views/HomePageActivity.kt
+++ app/src/main/java/com/gokulraj/neocare/views/HomePageActivity.kt
@@ -1,8 +1,11 @@
 package com.gokulraj.neocare.views
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.gokulraj.neocare.databinding.ActivityHomepageBinding
@@ -11,6 +14,11 @@ import kotlin.system.exitProcess
 class HomePageActivity:AppCompatActivity() {
 
     private lateinit var binding: ActivityHomepageBinding
+
+    //Declaring a request code for voice command
+
+    private val VOICE_COMMAND_REQUEST_CODE = 100
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +41,12 @@ class HomePageActivity:AppCompatActivity() {
             startActivity(Intent(this,RegistrationActivity::class.java))
         }
             */
+
+        // Attach a click listener to the voice command button
+        binding.voiceCommandButton.setOnClickListener {
+            startVoiceCommand()
+        }
+
         binding.teamMembersLink.setOnClickListener {
             startActivity(Intent(this, TeamMembersActivity::class.java))
         }
@@ -46,6 +60,36 @@ class HomePageActivity:AppCompatActivity() {
 
     }
 
+    private fun startVoiceCommand() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+
+        try {
+            startActivityForResult(intent, VOICE_COMMAND_REQUEST_CODE)
+        } catch (e: ActivityNotFoundException) {
+            // Handle the case where voice recognition is not supported on the device
+            Toast.makeText(this, "Voice recognition not supported on this device", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Handle the result of the voice command
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == VOICE_COMMAND_REQUEST_CODE && resultCode == RESULT_OK) {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val command = result?.get(0) // Get the recognized voice command
+
+            // Check the recognized voice command and perform the action
+            if (command.equals("call ambulance", ignoreCase = true)) {
+                // You can initiate a call to ambulance services here
+                val phoneNumber = "tel:911"
+                val callIntent = Intent(Intent.ACTION_DIAL, Uri.parse(phoneNumber))
+                startActivity(callIntent)
+            } else {
+                Toast.makeText(this, "Voice command not recognized or supported.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
     override fun onBackPressed() {
         AlertDialog.Builder(this)
             .setTitle("Exit App")
