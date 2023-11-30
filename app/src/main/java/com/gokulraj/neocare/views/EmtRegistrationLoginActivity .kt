db4bd6c899f87a3c404365dd176eb4a06428de82
Limboyanking <yanking3516@gmail.com>
2023-11-30 10:26:02 -0500

    emt loginRegistration

--- app/src/main/java/com/gokulraj/neocare/views/EmtRegistrationLoginActivity .kt
+++ app/src/main/java/com/gokulraj/neocare/views/EmtRegistrationLoginActivity .kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.gokulraj.neocare.R
 import com.gokulraj.neocare.database.User
 import com.gokulraj.neocare.database.EmergencyTechnicianEntity
+import com.gokulraj.neocare.databinding.ActivityEmtLoginBinding
+import com.gokulraj.neocare.databinding.ActivityEmtRegistrationBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -16,27 +19,30 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlin.system.exitProcess
 
+
+
 class EmtRegistrationLoginActivity : AppCompatActivity(), View.OnFocusChangeListener {
 
-    private lateinit var mBinding: ActivityEmtRegistrationLoginBinding
+    private lateinit var loginBinding: ActivityEmtLoginBinding
+    private lateinit var registerBinding: ActivityEmtRegistrationBinding
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
     private val userType: String = "EMT"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityEmtRegistrationLoginBinding.inflate(layoutInflater)
-        setContentView(mBinding.root)
+        loginBinding = ActivityEmtLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
 
         firebaseDatabase = FirebaseDatabase.getInstance()
         databaseReference = firebaseDatabase.reference.child("users")
 
-        mBinding.emailAddressEt.onFocusChangeListener = this
-        mBinding.passwordEt.onFocusChangeListener = this
+        loginBinding.emailAddressEt.onFocusChangeListener = this
+        loginBinding.passwordEt.onFocusChangeListener = this
 
-        mBinding.btnLogin.setOnClickListener {
-            val email = mBinding.emailAddressEt.text.toString()
-            val password = mBinding.passwordEt.text.toString()
+        loginBinding.btnLogin.setOnClickListener {
+            val email = loginBinding.emailAddressEt.text.toString()
+            val password = loginBinding.passwordEt.text.toString()
 
             if (validateEmail() && validatePassword()) {
                 loginUser(email, password)
@@ -45,15 +51,15 @@ class EmtRegistrationLoginActivity : AppCompatActivity(), View.OnFocusChangeList
             }
         }
 
-        mBinding.btnRegister.setOnClickListener {
+        registerBinding = ActivityEmtRegistrationBinding.inflate(layoutInflater)
+        registerBinding.btnRegister.setOnClickListener {
             // Handle EMT registration here
             // To create a separate registration process for EMTs
             // and store the registration details in the Firebase Realtime Database
 
-
-            val fullName = mBinding.fullNameEt.text.toString()
-            val email = mBinding.emailAddressEt.text.toString()
-            val password = mBinding.passwordEt.text.toString()
+            val fullName = registerBinding.fullNameEt.text.toString()
+            val email = registerBinding.emailAddressEt.text.toString()
+            val password = registerBinding.passwordEt.text.toString()
 
             // Implement EMT registration logic
             registerUser(fullName, email, password)
@@ -104,7 +110,7 @@ class EmtRegistrationLoginActivity : AppCompatActivity(), View.OnFocusChangeList
         // Implement email validation logic specific to EMT registration
         // To check if the email format is valid, if it's already registered, etc.
         // Return true if email is valid, false otherwise.
-        val email = mBinding.emailAddressEt.text.toString().trim()
+        val email = loginBinding.emailAddressEt.text.toString().trim()
         return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
@@ -112,18 +118,50 @@ class EmtRegistrationLoginActivity : AppCompatActivity(), View.OnFocusChangeList
         // Implement password validation logic specific to EMT registration
         // To check the password strength, length, etc.
         // Return true if the password is valid, false otherwise.
-        val password = mBinding.passwordEt.text.toString().trim()
+        val password = loginBinding.passwordEt.text.toString().trim()
         return password.isNotEmpty() && password.length >= 6
     }
 
     override fun onFocusChange(view: View?, hasFocus: Boolean) {
-        // Implement the onFocusChange logic for EMT registration fields
-        // To validate fields as the user interacts with them.
+        when (view?.id) {
+            R.id.emailAddressEt -> {
+                if (hasFocus) {
+                    // Email field focused, perform validation or show a hint if needed
+                    loginBinding.emailAddressTil.hint = "Enter your email"
+                } else {
+                    // Email field lost focus, reset hint if no text entered
+                    if (loginBinding.emailAddressEt.text.isNullOrEmpty()) {
+                        loginBinding.emailAddressTil.hint = ""
+                    }
+                }
+            }
+            R.id.passwordEt -> {
+                if (hasFocus) {
+                    // Password field focused, perform validation or show a hint if needed
+                    loginBinding.passwordTil.hint = "Enter your password"
+                } else {
+                    // Password field lost focus, reset hint if no text entered
+                    if (loginBinding.passwordEt.text.isNullOrEmpty()) {
+                        loginBinding.passwordTil.hint = ""
+                    }
+                }
+            }
+        }
     }
 
     override fun onBackPressed() {
         // Handle the back button behavior for EMT registration/login
         // To navigate back to the login screen or exit the app.
+        AlertDialog.Builder(this)
+            .setTitle("Exit Registration/Login")
+            .setMessage("Are you sure you want to exit the registration/login process?")
+            .setPositiveButton("Yes") { _, _ ->
+                // If the user clicks "Yes", redirect to the homepage activity
+                startActivity(Intent(this@EmtRegistrationLoginActivity, HomePageActivity::class.java))
+                finish()
+            }
+            .setNegativeButton("No", null)
+            .show()
     }
 }
 
